29f13fa7928bac021ff50e9c0feb8bf233a62bac
ivan_tustanivsky <tustanivsky@gmail.com>
2018-02-17 11:41:56 +0200

    Class representing tank track was created * Added new TankTrack class * In tank bluepring default static mesh tracks were repleced with newly created custom tracks

---
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
