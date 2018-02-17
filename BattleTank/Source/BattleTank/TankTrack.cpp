313c8123149372a7f6655c35bb5dc5a14daf7feb
ivan_tustanivsky <tustanivsky@gmail.com>
2018-02-17 12:08:26 +0200

    Throttle input mapping was implemented * Added new input mappings for throttling left and right trach with gamepad triggers to project settings * Tank blueprint input setup graph modified * Added SetThrottle blueprint callable method to tank track class

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -1,5 +1,8 @@
 #include "TankTrack.h"
 
+void UTankTrack::SetThrottle(float Throttle)
+{
+	UE_LOG(LogTemp, Warning, TEXT("%s is throttling %f"), *GetName(), Throttle);
 
-
-
+	// TODO Clamp Throttle value
+}
