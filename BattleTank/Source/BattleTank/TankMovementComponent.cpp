e3af57e3b5401361662152b99ce88aa0427e9449
ivan_tustanivsky <tustanivsky@gmail.com>
2018-02-20 08:53:43 +0200

    For AI conrolled tanks possibility to move towards player  was added * Added calculation of throw that should be applied to enemy tank to move towards player (dot product of tank direction vector and directon towards player) * For now enemy tanks move only back and forward

--- BattleTank/Source/BattleTank/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/TankMovementComponent.cpp
@@ -41,5 +41,10 @@ void UTankMovementComponent::IntendMoveAside(float Throw)
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("%s direct move velocity %s"), *GetOwner()->GetName(), *MoveVelocity.ToString());
+	FVector TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	FVector AIForwardIntention = MoveVelocity.GetSafeNormal();
+
+	float ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+
+	IntendMoveForward(ForwardThrow);
 }
