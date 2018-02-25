6c1db7f70e0055148b64db109423816251566b50
ivan_tustanivsky <tustanivsky@gmail.com>
2018-02-25 16:04:56 +0200

    More issues with tank movement were fixed * Added damping for tank movement * Now tank is able to move only when its tracks staying on the ground * Tank Track class refactored

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -4,22 +4,30 @@ UTankTrack::UTankTrack()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UTankTrack::BeginPlay()
+{
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
 }
 
 void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	
+}
+
+void UTankTrack::ApplySidewayForce()
+{
 	// Calculate slippage speed
 	float SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
 
 	// Work-out required acceleration this frame to correct
-	FVector CorrectionAcceleration = -(SlippageSpeed / DeltaTime * GetRightVector());
+	FVector CorrectionAcceleration = -(SlippageSpeed / GetWorld()->GetDeltaSeconds() * GetRightVector());
 
 	// Get reference to tank body
 	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
-	
+
 	// Calculate correction sideway force ( F = m * a)
 	FVector CorrectionForce = CorrectionAcceleration * TankRoot->GetMass() / 2;
 
@@ -29,9 +37,14 @@ void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActor
 
 void UTankTrack::SetThrottle(float Throttle)
 {
+	CurrentThrottle = FMath::Clamp(CurrentThrottle + Throttle, -1.0f, 1.0f);
+}
+
+void UTankTrack::DriveTrack()
+{
 	// Calculate force that should be applied to move tank
 	// Throttle sign also determines movement direction (negative for backward and positive for forward)
-	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	FVector ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
 
 	// Get location at which force should be applied
 	// It is determined by socket which is used to attach track to tank body
@@ -42,4 +55,14 @@ void UTankTrack::SetThrottle(float Throttle)
 
 	// Apply force to tank body to make it move
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLoaction);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	DriveTrack();
+
+	ApplySidewayForce();
+
+	// Reset current throttle
+	CurrentThrottle = 0.0f;
 }
--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -24,17 +24,34 @@ public:
 	UTankTrack();
 
 protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 	// Function called every frame on this ActorComponent
 	void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
 
-public:
-	// Apply force to move tank
-	// Movement speed and direction depends on throttle value
+	// Apply force for tank slippage compensation
+	void ApplySidewayForce();
+
+public:	
+	// Set track throttle value that defines movement speed and direction
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
+
+	// Apply force to track to move tank	
+	void DriveTrack();
+
+private:
+	// Handle hit event to perform necessary movement operations
+	// It is called only when tank track stays on the ground (hits the landscape)
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 	
 private:
 	// Maximum force that track can apply to tank when throttle (in Newtons)
 	UPROPERTY(EditDefaultsOnly)
 	float TrackMaxDrivingForce = 400000.0f;
+
+	// Current throttle value
+	float CurrentThrottle = 0.0f;
 };
